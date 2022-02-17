3bad4403785b13e1cc8af529577f0bff0cf7ef37
Michael <knappmi14@gmail.com>
2022-02-17 14:17:19 -0500

    nornir

---
+++ helpers.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from nornir import InitNornir
+
+# Load and populate the environment variables from .env
+load_dotenv()
+
+# Set the path to the Nornir config file
+NORNIR_CONFIG_FILE = f"{Path(__file__).parent.parent}\\nornir\\nr-config.yml"
+
+# Create a Nornir setup function
+def nornir_setup():
+    # Initialize Nornir
+    nr = InitNornir(config_file=NORNIR_CONFIG_FILE)
+
+    # Set the username and password using the environment variables
+    nr.inventory.defaults.username = os.getenv("DEVICE_USERNAME")
+    nr.inventory.defaults.password = os.getenv("DEVICE_PASSWORD")
+
+    return nr
--- nornir_test.py
+++ nornir_test.py
@@ -1,5 +1,31 @@
+# Perform the required imports
+from helpers import nornir_setup
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.files import write_file
 
+# Perform the Nornir initialization
+nr = nornir_setup()
 
-from nornir import InitNornir
-nr = InitNornir(config_file = './nr-config.yml')
+# Set the path to where we want the backups to be saved
+BACKUP_PATH = "./data/configs"
 
+# Create a Nornir Task.
+def backup_config(task, path):
+    # Task 1. Run the NAPALM config getter to collect the config
+    device_config = task.run(task=napalm_get, getters=["get_config"])
+
+    # Task 2. Write the device config to a file using the Nornir, write_file task
+    task.run(
+        task=write_file,
+        content=device_config.result["config"]["running"],
+        filename=f"{path}/{task.host}.txt",
+    )
+
+# Run our back_config task against all of our devices. We provide the backup path and also the task name we want to run against all the devices.
+result = nr.run(
+    name="Backup Device configurations", path=BACKUP_PATH, task=backup_config
+)
+
+# Finally, we print the results of running our task against all the devices.
+print_result(result, vars=["stdout"])
